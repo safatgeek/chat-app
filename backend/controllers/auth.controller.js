df960aa462c8c1ad70bc2cb03a823ca8c64ec1f8
safatgeek <safatd310e@gmail.com>
2024-04-02 23:55:27 +0600

    auth-controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export const signup =  (req, res) => {
+    res.send("signup Route");
+    console.log("Signup user")
+};
+
+export const login =  (req, res) => {
+    res.send("login Route");
+    console.log("Login user")
+};
+
+export const logout =  (req, res) => {
+    res.send("logout Route");
+    console.log("Logout user")
+};
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+// import authRoutes from "./routes/auth.routes.js";
+import router from "./routes/auth.routes.js";
 
 const app = express();
+
+dotenv.config();
+const PORT = process.env.PORTT || 5000;
 
 app.get("/", (req,res) => {
     res.send("Hello world !!!!!!!!!!!!")
 })
 
-app.listen(5000, () => console.log("Server Running on port 5000"))
+app.use("/api/auth", router)
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`))
