09d3a7fcc4a962c58f074b025aecd59e6d4f5408
safatgeek <safatd310e@gmail.com>
2024-04-10 17:11:02 +0600

    protect route middleware & conversation

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongodb.js";
+import messageRoutes from "./routes/message.routes.js";
+import cookieParser from "cookie-parser";
+import protectRoute from "./middleware/protectRoute.js";
 
 const app = express();
 
@@ -9,12 +12,15 @@ dotenv.config();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 // app.get("/", (req,res) => {
 //     res.send("Hello world !!!!!!!!!!!!")
 // })
 
 app.use("/api/auth", authRoutes)
+app.use("/api/messages",protectRoute, messageRoutes)
+
 
 app.listen(PORT, () => {
     connectToMongoDB()
