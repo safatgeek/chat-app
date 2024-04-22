40eb1e7fbc38e3318ebaba399ac9b51e51539aff
safatgeek <safatd310e@gmail.com>
2024-04-22 22:52:21 +0600

    conversations

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Sidebar from '../../component/sidebar/Sidebar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+      <Sidebar />
+    </div>
   )
 }
 
