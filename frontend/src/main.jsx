9345d4bf606c2cc58a8ebc53aa867564839d0679
safatgeek <safatd310e@gmail.com>
2024-06-14 16:13:04 +0600

    bg, socket url problem fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./jsx";
 import "./index.css";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
 import { SocketContextProvider } from "./context/SocketContext.jsx";
