2bb9f12b177999e8381ec4e394c75b35a0ebd473
safatgeek <safatd310e@gmail.com>
2024-06-14 16:52:00 +0600

    Fixes

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -6,14 +6,16 @@ import notificationSound from "../assets/sound/notification.mp3";
 
 const useListenMessages = () => {
   const { socket } = useSocketContext();
-  const { messages, setMessages } = useConversation();
+  const { messages, setMessages, selectedConversation } = useConversation();
 
   useEffect(() => {
     socket?.on("newMessage", (newMessage) => {
-      newMessage.shouldShake = true;
-      const sound = new Audio(notificationSound);
-      sound.play()
-      setMessages([...messages, newMessage]);
+      if (newMessage.senderId == selectedConversation._id) {
+        newMessage.shouldShake = true;
+        const sound = new Audio(notificationSound);
+        sound.play();
+        setMessages([...messages, newMessage]);
+      }
     });
 
     return () => socket?.off("newMessage");
