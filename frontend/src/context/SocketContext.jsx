3da5169809817d75b8f74d1b98b21444be05ec88
safatgeek <safatd310e@gmail.com>
2024-06-14 16:27:56 +0600

    live url added to socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -18,7 +18,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://chat-app-bd-uqof.onrender.com", {
                 query:{
                     userId: authUser._id
                 }
